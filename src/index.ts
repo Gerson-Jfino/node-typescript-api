601b141c4e3c06f78617492d097450daed0d0487
Gerson Maoze <gersonjfmaoze@gmail.com>
2023-04-10 17:55:52 +0200

    feat: code style configs

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,5 @@
 import { server } from './server/Server';
 
 server.listen(3000, () => {
-    console.log(`server working on port: 3000`);
+    console.log('server working on port: 3000');
 });
--- src/server/Server.ts
+++ src/server/Server.ts
@@ -3,7 +3,6 @@ import express from 'express';
 const server = express();
 
 server.get('/', (req, res) => {
-    res.send("<h1>Funcionando</h1>")
+    res.send("<h1>Funcionando</h1>");
 });
-
 export { server };
